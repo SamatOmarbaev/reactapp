c29b25d62575c8a859ba91ea4ae6bbdb6be8448e
Samat <xzwqt@mail.ru>
2023-11-09 15:40:36 +0700

    добавлена сортировка поиском и через select

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import Title from "../Title/Title";
 import './style.css';
-import Loader from "../UI/Loader/Loader";
 import PostItem from "../PostItem/PostItem";
+import Title from "../Title/Title";
 
 const PostList = ({posts}) => {
 
     if (!posts.length) {
-        return <Loader />
+        return <Title children={'Постов не найдено'} />
     }
 
     return (
         <>
-            <Title children={'Список постов'} />
             <div className="post__list">
                 {posts.map((post =>
                     <PostItem post={post} key={post.id} />
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import PostList from "../components/PostList/PostList";
 import { PostService } from "../API/postService";
+import Title from "../components/Title/Title";
+import Loader from '../components/UI/Loader/Loader';
+import PostFilter from "../components/PostFilter/PostFilter";
 
 const Posts = () => {
     const [posts, setPosts] = useState([]);
+    const [filter, setFilter] = useState({sort: '', query: ''});
 
     const getPosts = async () => {
         const response = await PostService.getAll()
@@ -14,9 +18,29 @@ const Posts = () => {
         getPosts();
     }, []);
 
+    const sortedPosts = useMemo(() => {
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+        }
+
+        return posts;
+    }, [filter.sort, posts]);
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
+    }, [filter.query, sortedPosts]);
+
     return (
         <div className="container">
-            <PostList posts={posts} />
+            {posts.length > 0
+                ?   <>
+                        <Title children={'Список постов'} />
+                        <PostFilter filter={filter} setFilter={setFilter} />
+                        <PostList posts={sortedAndSearchedPosts} />
+                    </>
+                :   <Loader />
+            }
+            
         </div>
     );
 }
